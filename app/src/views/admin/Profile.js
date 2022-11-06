5efb15c2172dd9fd8df03c55026c9508fe68d7f0
Ronald <rjdiazi10@gmail.com>
2022-11-05 22:53:39 -0500

    layout for user profile form

--- app/src/views/admin/Profile.js
+++ app/src/views/admin/Profile.js
@@ -1,69 +1,48 @@
-import { Avatar, Box, Button, Center, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react'
+import { Button, Flex, FormControl, FormLabel, Heading, Input, Stack } from '@chakra-ui/react'
 
 const Profile = () => {
   return (
-    <Center>
-      <Box maxW={'270px'} w={'full'} bg={'white'} boxShadow={'2xl'} rounded={'md'} overflow={'hidden'}>
-        <Image
-          h={'120px'}
-          w={'full'}
-          src={
-            'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-          }
-          objectFit={'cover'}
-        />
-        <Flex justify={'center'} mt={-12}>
-          <Avatar
-            size={'xl'}
-            src={
-              'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
-            }
-            alt={'Author'}
-            css={{
-              border: '2px solid white'
-            }}
-          />
-        </Flex>
-
-        <Box p={6}>
-          <Stack spacing={0} align={'center'} mb={5}>
-            <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
-              John Doe
-            </Heading>
-            <Text color={'gray.500'}>Frontend Developer</Text>
-          </Stack>
-
-          <Stack direction={'row'} justify={'center'} spacing={6}>
-            <Stack spacing={0} align={'center'}>
-              <Text fontWeight={600}>23k</Text>
-              <Text fontSize={'sm'} color={'gray.500'}>
-                Followers
-              </Text>
-            </Stack>
-            <Stack spacing={0} align={'center'}>
-              <Text fontWeight={600}>23k</Text>
-              <Text fontSize={'sm'} color={'gray.500'}>
-                Followers
-              </Text>
-            </Stack>
-          </Stack>
-
+    <Flex justify="center">
+      <Stack spacing={4} w="full" maxW="md" bg="white" rounded="xl" boxShadow="lg" p={6}>
+        <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
+          Mi Perfil
+        </Heading>
+        <FormControl id="name" isRequired>
+          <FormLabel>Nombre</FormLabel>
+          <Input type="text" />
+        </FormControl>
+        <FormControl id="userName" isRequired>
+          <FormLabel>Usuario</FormLabel>
+          <Input type="text" />
+        </FormControl>
+        <FormControl id="password" isRequired>
+          <FormLabel>Contraseña</FormLabel>
+          <Input type="password" />
+        </FormControl>
+        <Stack spacing={6} direction={['column', 'row']}>
           <Button
-            w={'full'}
-            mt={8}
-            bg={'#151f21'}
+            bg={'blue.400'}
             color={'white'}
-            rounded={'md'}
+            w="full"
             _hover={{
-              transform: 'translateY(-2px)',
-              boxShadow: 'lg'
+              bg: 'blue.500'
             }}
           >
-            Follow
+            Guardar
           </Button>
-        </Box>
-      </Box>
-    </Center>
+          <Button
+            bg={'red.400'}
+            color={'white'}
+            w="full"
+            _hover={{
+              bg: 'red.500'
+            }}
+          >
+            Cancelar
+          </Button>
+        </Stack>
+      </Stack>
+    </Flex>
   )
 }
 
