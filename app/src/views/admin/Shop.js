5749d27ef2e2f0d2ab5b4c2db6c7e8712c0772b3
Ronald <rjdiazi10@gmail.com>
2022-11-19 23:06:01 -0500

    added profile and headboard images to shop form

--- app/src/views/admin/Shop.js
+++ app/src/views/admin/Shop.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Button, Flex, Heading, InputLeftAddon, Stack, Textarea, useToast } from '@chakra-ui/react'
+import { Avatar, Box, Button, Flex, Heading, Image, InputLeftAddon, Stack, Textarea, useToast } from '@chakra-ui/react'
 import { Field, Form, Formik } from 'formik'
 import PropTypes from 'prop-types'
 import CircularSpinner from '../../components/feedback/CircularSpinner'
@@ -22,11 +22,13 @@ const Shop = ({ shopId }) => {
 
   const handleSubmit = async values => {
     try {
-      const { id } = await request(`/companies/${shopId}`, 'PATCH', {
+      const response = await request(`/companies/${shopId}`, 'PATCH', {
         ...values,
         cellPhone: `+51${values.cellPhone}`
       })
-      if (id) {
+
+      if (response.id) {
+        setShopData({ ...shopData, images: response.images })
         showToast(
           toast,
           setToastContent('Éxito', 'Datos de tienda actualizados correctamente', 'success', 'subtle', 'top')
@@ -55,6 +57,8 @@ const Shop = ({ shopId }) => {
     })
   }
 
+  const getPictureUrl = type => shopData.images.find(item => item.type === type).url
+
   useEffect(() => {
     getShopData()
   }, [shopId])
@@ -63,66 +67,72 @@ const Shop = ({ shopId }) => {
 
   return (
     <Flex justify="center">
-      <Stack spacing={4} w="full" maxW="md" bg="white" rounded="xl" boxShadow="lg" p={6}>
-        <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
-          Mi Tienda
-        </Heading>
-        <Formik initialValues={shopData} onSubmit={handleSubmit} enableReinitialize>
-          {({ values, handleChange }) => (
-            <Form>
-              <Stack spacing={4}>
-                <TextField name="name" label="Nombre" validate={validateRequired} />
-                <Field name="description" validate={validateRequired}>
-                  {({ field, meta }) => (
-                    <FormField label="Descripción" meta={meta}>
-                      <Textarea resize="none" {...field} />
-                    </FormField>
-                  )}
-                </Field>
-                <TextField name="address" label="Dirección" validate={validateRequired} />
-                <TextField name="placeService" label="Lugar de servicio" validate={validateRequired} />
-                <TextField
-                  name="cellPhone"
-                  label="Teléfono Móvil"
-                  validate={validateRequired}
-                  inputAddons={<InputLeftAddon>+51</InputLeftAddon>}
-                />
-                <ArrayField
-                  name="attentionSchedule"
-                  label="Horarios de atención"
-                  values={values.attentionSchedule}
-                  fields={{ day: '', schedule: '' }}
-                  fieldsPlaceholder={{ day: 'Día(s)', schedule: 'Horario' }}
-                  validate={validateRequired}
-                />
-                <ArraySelectField
-                  name="socialNetworks"
-                  label="Redes sociales"
-                  values={values.socialNetworks}
-                  fields={{ type: '', url: '' }}
-                  fieldsPlaceholder={{ url: 'Enlace' }}
-                  options={socialNetworksOptions}
-                  validate={validateRequired}
-                  handleSelectChange={handleChange}
-                />
-                <ArraySelectField
-                  name="images"
-                  label="Imágenes"
-                  values={values.images}
-                  fields={{ type: '', url: '' }}
-                  fieldsPlaceholder={{ url: 'Enlace' }}
-                  options={shopImageOptions}
-                  validate={validateRequired}
-                  handleSelectChange={handleChange}
-                />
-                <Button bg="blue.400" color="white" w="full" _hover={{ bg: 'blue.500' }} type="submit">
-                  Guardar
-                </Button>
-              </Stack>
-            </Form>
-          )}
-        </Formik>
-      </Stack>
+      <Box w="full" maxW="md" bg="white" rounded="xl" boxShadow="lg" overflow="hidden">
+        <Image height="150px" w="full" src={getPictureUrl('headboard')} objectFit="cover" />
+        <Flex justify="center" marginTop={-12}>
+          <Avatar size="2xl" src={getPictureUrl('profile')} alt="Shop" borderWidth="initial" borderColor="white" />
+        </Flex>
+        <Stack spacing={4} padding={6}>
+          <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
+            Mi Tienda
+          </Heading>
+          <Formik initialValues={shopData} onSubmit={handleSubmit} enableReinitialize>
+            {({ values, handleChange }) => (
+              <Form>
+                <Stack spacing={4}>
+                  <TextField name="name" label="Nombre" validate={validateRequired} />
+                  <Field name="description" validate={validateRequired}>
+                    {({ field, meta }) => (
+                      <FormField label="Descripción" meta={meta}>
+                        <Textarea resize="none" {...field} />
+                      </FormField>
+                    )}
+                  </Field>
+                  <TextField name="address" label="Dirección" validate={validateRequired} />
+                  <TextField name="placeService" label="Lugar de servicio" validate={validateRequired} />
+                  <TextField
+                    name="cellPhone"
+                    label="Teléfono Móvil"
+                    validate={validateRequired}
+                    inputAddons={<InputLeftAddon>+51</InputLeftAddon>}
+                  />
+                  <ArrayField
+                    name="attentionSchedule"
+                    label="Horarios de atención"
+                    values={values.attentionSchedule}
+                    fields={{ day: '', schedule: '' }}
+                    fieldsPlaceholder={{ day: 'Día(s)', schedule: 'Horario' }}
+                    validate={validateRequired}
+                  />
+                  <ArraySelectField
+                    name="socialNetworks"
+                    label="Redes sociales"
+                    values={values.socialNetworks}
+                    fields={{ type: '', url: '' }}
+                    fieldsPlaceholder={{ url: 'Enlace' }}
+                    options={socialNetworksOptions}
+                    validate={validateRequired}
+                    handleSelectChange={handleChange}
+                  />
+                  <ArraySelectField
+                    name="images"
+                    label="Imágenes"
+                    values={values.images}
+                    fields={{ type: '', url: '' }}
+                    fieldsPlaceholder={{ url: 'Enlace' }}
+                    options={shopImageOptions}
+                    validate={validateRequired}
+                    handleSelectChange={handleChange}
+                  />
+                  <Button bg="blue.400" color="white" w="full" _hover={{ bg: 'blue.500' }} type="submit">
+                    Guardar
+                  </Button>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
+        </Stack>
+      </Box>
     </Flex>
   )
 }
