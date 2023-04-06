8531d4d5906114b23b24ba69c106f913a0d5598a
ronaldidro <rjdiazi10@gmail.com>
2023-04-06 13:14:46 -0500

    fixed reset endpoint for test

--- api/controllers/testing.js
+++ api/controllers/testing.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
 const Company = require('../models/company')
+const Product = require('../models/product')
+const ProductCategory = require('../models/product-category')
 
 router.post('/reset', async (request, response) => {
   await Company.deleteMany({})
+  await ProductCategory.deleteMany({})
+  await Product.deleteMany({})
 
   response.status(204).end()
 })
