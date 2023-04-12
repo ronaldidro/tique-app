fefde67e667af8872716920a66587082107eec70
ronaldidro <rjdiazi10@gmail.com>
2023-04-12 01:16:48 -0500

    updated order detail page

--- app/src/views/market/OrderDetail.js
+++ app/src/views/market/OrderDetail.js
@@ -1,6 +1,21 @@
 import { useNavigate } from 'react-router-dom'
-import { Badge, Container, Divider, Flex, Heading, IconButton, Spacer, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Divider,
+  Flex,
+  HStack,
+  Heading,
+  Icon,
+  IconButton,
+  Spacer,
+  Tag,
+  Text
+} from '@chakra-ui/react'
 import { ChevronLeftIcon } from '@chakra-ui/icons'
+import { TbShoppingCartX } from 'react-icons/tb'
 import OrderFormButton from '../../components/market/OrderFormButton'
 import OrderList from '../../components/market/OrderList'
 import { getProductsOrder, getOrderTotalPrice, getOrderTotalItems } from '../../utils'
@@ -14,28 +29,44 @@ const OrderDetail = () => {
   return (
     <Container maxW="5xl">
       <Flex alignItems="center" paddingY={4}>
-        <IconButton icon={<ChevronLeftIcon />} onClick={() => navigate(-1)} />
+        <IconButton icon={<ChevronLeftIcon boxSize={7} />} onClick={() => navigate(-1)} />
         <Heading size="md" paddingLeft={4}>
-          Tu Pedido
+          Tu pedido
         </Heading>
         <Spacer />
-        <Flex gap={2}>
-          <Text as="em">Items:</Text>
-          <Badge fontSize="md">{orderTotalItems}</Badge>
-          <Text as="em">Total:</Text>
-          <Badge fontSize="md" colorScheme="green">
-            S/ {orderTotalPrice.toFixed(2)}
-          </Badge>
-        </Flex>
+        <Box gap={[2, 4]} display={{ base: 'grid', md: 'flex' }} justifyItems="end">
+          <HStack>
+            <Text as="b">Artículos:</Text>
+            <Tag fontWeight="bold" colorScheme="orange">
+              {orderTotalItems}
+            </Tag>
+          </HStack>
+          <HStack>
+            <Text as="b">Total:</Text>
+            <Tag fontWeight="bold" colorScheme="green">
+              S/ {orderTotalPrice.toFixed(2)}
+            </Tag>
+          </HStack>
+        </Box>
       </Flex>
       <Divider />
-      <OrderList productsOrder={productsOrder} />
       {productsOrder.length > 0 ? (
-        <OrderFormButton />
+        <Box display="grid" alignContent="space-between" minHeight="xl">
+          <OrderList productsOrder={productsOrder} />
+          <OrderFormButton />
+        </Box>
       ) : (
-        <Text textAlign="center" fontWeight="bold" marginY={5}>
-          No se encontraron productos
-        </Text>
+        <Center minHeight="xl">
+          <Box textAlign="center">
+            <Icon as={TbShoppingCartX} boxSize={40} />
+            <Text fontWeight="bold" marginY={5}>
+              El carrito está vacío
+            </Text>
+            <Button colorScheme="facebook" onClick={() => navigate(-1)}>
+              Regresar
+            </Button>
+          </Box>
+        </Center>
       )}
     </Container>
   )
