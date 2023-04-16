0395a4f6f59644a5cff0940d53331c7a3990b814
ronaldidro <rjdiazi10@gmail.com>
2023-04-15 20:09:44 -0500

    added set filter value

--- app/src/components/fields/SearchField.js
+++ app/src/components/fields/SearchField.js
@@ -2,19 +2,18 @@ import { SearchIcon } from '@chakra-ui/icons'
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
 
-const SearchField = ({ placeholder = 'Buscar', handleSearch, ...inputProps }) => {
-  return (
-    <InputGroup {...inputProps}>
-      <InputLeftElement pointerEvents="none">
-        <SearchIcon />
-      </InputLeftElement>
-      <Input placeholder={placeholder} onChange={handleSearch} />
-    </InputGroup>
-  )
-}
+const SearchField = ({ placeholder = 'Buscar', value, handleSearch, ...inputProps }) => (
+  <InputGroup {...inputProps}>
+    <InputLeftElement pointerEvents="none">
+      <SearchIcon />
+    </InputLeftElement>
+    <Input placeholder={placeholder} value={value} onChange={handleSearch} />
+  </InputGroup>
+)
 
 SearchField.propTypes = {
   placeholder: PropTypes.string,
+  value: PropTypes.string,
   handleSearch: PropTypes.func
 }
 
--- app/src/components/market/Filters.js
+++ app/src/components/market/Filters.js
@@ -1,11 +1,12 @@
 import { Box, Select } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { filterChange } from '../../reducers/filterReducer'
 import { getProducts } from '../../utils'
 import SearchField from '../fields/SearchField'
 
 const Filters = () => {
+  const { mode, content } = useSelector(state => state.filter)
   const dispatch = useDispatch()
 
   const handleCategoryChange = ({ target }) =>
@@ -18,12 +19,18 @@ const Filters = () => {
     <Box display={{ md: 'flex' }} marginY={2}>
       <SearchField
         placeholder="Buscar producto"
+        value={mode === 'BY_NAME' ? content : ''}
         handleSearch={handleProductSearch}
         width={['100%', '65%']}
         marginRight={[0, 2]}
         marginBottom={[2, 0]}
       />
-      <Select placeholder="Categorías" width={['100%', '35%']} onChange={handleCategoryChange}>
+      <Select
+        placeholder="Todas las categorías"
+        onChange={handleCategoryChange}
+        value={mode === 'BY_CATEGORY' ? content : ''}
+        width={['100%', '35%']}
+      >
         {getProducts().map(category => (
           <option key={category.id} value={category.id}>
             {category.description}
