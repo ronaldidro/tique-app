e4e67328f03ce512b304a7140e315e62acae07c3
ronaldidro <rjdiazi10@gmail.com>
2023-04-15 19:56:24 -0500

    removed not found products

--- app/src/components/market/Categories.js
+++ app/src/components/market/Categories.js
@@ -1,17 +1,8 @@
-import { Text } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
 import Category from './Category'
 
-const Categories = ({ categoriesData }) => {
-  if (!categoriesData.length)
-    return (
-      <Text textAlign="center" fontWeight="bold" marginY={5}>
-        No se encontraron productos
-      </Text>
-    )
-
-  return categoriesData.map(category => <Category key={category.id} categoryData={category} />)
-}
+const Categories = ({ categoriesData }) =>
+  categoriesData.map(category => <Category key={category.id} categoryData={category} />)
 
 Categories.propTypes = {
   categoriesData: PropTypes.array
