f4ecd9c2c5a58fa41a301c70ea03b09c957e6d05
ronaldidro <rjdiazi10@gmail.com>
2023-04-22 12:23:47 -0500

    updated market footer component

--- app/src/components/market/Footer.js
+++ app/src/components/market/Footer.js
@@ -1,15 +1,43 @@
-import { Box, Container, Text } from '@chakra-ui/react'
+import { ButtonGroup, Container, Flex, IconButton, Stack, Text } from '@chakra-ui/react'
+import { FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import AppLogo from '../AppLogo'
 
 const Footer = () => (
-  <Box backgroundColor="gray.50">
-    <Container maxW="5xl">
-      <Box textAlign="right" borderTop="1px" borderColor="blackAlpha.900">
-        <Text fontSize={['sm', 'md']} paddingTop={2}>
-          &copy; {new Date().getFullYear()} Hatun Tech. Todos los derechos reservados.
-        </Text>
-      </Box>
-    </Container>
-  </Box>
+  <Container maxW="5xl" as="footer" paddingY={5}>
+    <Stack spacing={{ base: '4', md: '5' }}>
+      <Stack justify="space-between" direction="row" align="center">
+        <Flex alignItems="center">
+          <AppLogo />
+        </Flex>
+        <ButtonGroup variant="ghost">
+          <IconButton
+            as="a"
+            href="https://www.linkedin.com/in/ronaldidro/"
+            target="_blank"
+            aria-label="LinkedIn"
+            icon={<FaLinkedin fontSize="1.25rem" />}
+          />
+          <IconButton
+            as="a"
+            href="https://www.instagram.com/ronaldidro/"
+            target="_blank"
+            aria-label="Instagram"
+            icon={<FaInstagram fontSize="1.25rem" />}
+          />
+          <IconButton
+            as="a"
+            href="https://twitter.com/ronaldidro"
+            target="_blank"
+            aria-label="Twitter"
+            icon={<FaTwitter fontSize="1.25rem" />}
+          />
+        </ButtonGroup>
+      </Stack>
+      <Text fontSize="sm" color="subtle">
+        &copy; {new Date().getFullYear()} Hatun Tec. Todos los derechos reservados.
+      </Text>
+    </Stack>
+  </Container>
 )
 
 export default Footer
