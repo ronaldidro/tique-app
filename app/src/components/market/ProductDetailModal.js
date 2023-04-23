d7f49a7ef7e34340e177c584c6b514c349135983
ronaldidro <rjdiazi10@gmail.com>
2023-04-23 17:21:04 -0500

    added toast  when product is added

--- app/src/components/market/ProductDetailModal.js
+++ app/src/components/market/ProductDetailModal.js
@@ -1,15 +1,16 @@
-import { useDispatch } from 'react-redux'
-import { IconButton, ModalBody, useDisclosure } from '@chakra-ui/react'
 import { AddIcon } from '@chakra-ui/icons'
+import { IconButton, ModalBody, useDisclosure, useToast } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
 import { addProduct, updateProduct } from '../../reducers/productsOrderReducer'
-import { getProductOrderById } from '../../utils'
+import { getProductOrderById, setToastContent, showToast } from '../../utils'
 import ModalDialog from '../overlay/ModalDialog'
 import ProductDetail from './ProductDetail'
 
 const ProductDetailModal = ({ productData }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const dispatch = useDispatch()
+  const toast = useToast()
   const productInOrder = getProductOrderById(productData.id)
 
   const addOrderProduct = event => {
@@ -19,11 +20,15 @@ const ProductDetailModal = ({ productData }) => {
     const discountedPrice = parseFloat(event.target.discountedPrice.value)
     const totalPrice = parseFloat(event.target.totalPrice.value)
     const comments = event.target.comments.value
-
     const productOrder = { ...productData, quantity, discountedPrice, totalPrice, comments }
 
     dispatch(productInOrder ? updateProduct(productOrder) : addProduct(productOrder))
-
+    showToast(
+      toast,
+      setToastContent('Se añadió al carrito', `${quantity} ${productData.name}`, 'success', 'subtle', 'top', {
+        isClosable: true
+      })
+    )
     onClose()
   }
 
