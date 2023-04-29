e8d210874f2fe4b89639c508bd15d1204ddd5cf9
ronaldidro <rjdiazi10@gmail.com>
2023-04-29 01:34:58 -0500

    added accordion for categories

--- app/src/components/market/Categories.js
+++ app/src/components/market/Categories.js
@@ -1,8 +1,33 @@
+import { AddIcon, MinusIcon } from '@chakra-ui/icons'
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Box } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
 import Category from './Category'
 
-const Categories = ({ categoriesData }) =>
-  categoriesData.map(category => <Category key={category.id} categoryData={category} />)
+const Categories = ({ categoriesData }) => (
+  <Box paddingBottom={8}>
+    {categoriesData.map(category => (
+      <Accordion key={category.id} defaultIndex={[0]} allowMultiple>
+        <AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left" fontSize="lg">
+                    {category.description}
+                  </Box>
+                  {isExpanded ? <MinusIcon boxSize={3} /> : <AddIcon boxSize={3} />}
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={7}>
+                <Category categoryData={category} />
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+      </Accordion>
+    ))}
+  </Box>
+)
 
 Categories.propTypes = {
   categoriesData: PropTypes.array
--- app/src/components/market/Category.js
+++ app/src/components/market/Category.js
@@ -1,26 +1,13 @@
-import { Box, Heading, SimpleGrid } from '@chakra-ui/react'
+import { SimpleGrid } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
 import ProductCard from './ProductCard'
 
 const Category = ({ categoryData }) => (
-  <Box paddingY={5}>
-    <Heading as="h2" fontSize="xl">
-      {categoryData.description}
-    </Heading>
-    <SimpleGrid
-      columns={{ base: 2, md: 4 }}
-      columnGap={{ base: 4, md: 6 }}
-      rowGap={{ base: 8, md: 10 }}
-      paddingTop={3}
-      width="full"
-      borderTop="1px"
-      borderColor="blackAlpha.500"
-    >
-      {categoryData.products.map(product => (
-        <ProductCard key={product.id} productData={product} />
-      ))}
-    </SimpleGrid>
-  </Box>
+  <SimpleGrid columns={{ base: 2, md: 4 }} columnGap={{ base: 4, md: 6 }} rowGap={{ base: 8, md: 10 }} width="full">
+    {categoryData.products.map(product => (
+      <ProductCard key={product.id} productData={product} />
+    ))}
+  </SimpleGrid>
 )
 
 Category.propTypes = {
