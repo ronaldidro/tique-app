4936d0c6e207b79239ad001e3afbeb754977d3dd
ronaldidro <rjdiazi10@gmail.com>
2023-04-29 19:05:30 -0500

    removed unused props

--- app/src/components/overlay/ModalDialog.js
+++ app/src/components/overlay/ModalDialog.js
@@ -1,24 +1,20 @@
 import { Modal, ModalCloseButton, ModalContent, ModalOverlay } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
 
-const ModalDialog = ({ children, isOpen, onClose, ...props }) => {
-  return (
-    <Modal isOpen={isOpen} onClose={onClose} {...props}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalCloseButton backgroundColor="whiteAlpha.500" zIndex={2} />
-        {children}
-      </ModalContent>
-    </Modal>
-  )
-}
+const ModalDialog = ({ children, isOpen, onClose, ...props }) => (
+  <Modal isOpen={isOpen} onClose={onClose} {...props}>
+    <ModalOverlay />
+    <ModalContent>
+      <ModalCloseButton backgroundColor="whiteAlpha.500" zIndex={2} />
+      {children}
+    </ModalContent>
+  </Modal>
+)
 
 ModalDialog.propTypes = {
   children: PropTypes.node,
   isOpen: PropTypes.bool,
-  onClose: PropTypes.func,
-  size: PropTypes.node,
-  isCentered: PropTypes.bool
+  onClose: PropTypes.func
 }
 
 export default ModalDialog
