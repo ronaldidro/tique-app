16d79b9e46656d7f408013a4c8f4a68d6319f453
ronaldidro <rdiaz@digital.gob.pe>
2023-05-01 17:32:39 -0500

    set false to devtools in production enviroment

--- app/src/store.js
+++ app/src/store.js
@@ -1,4 +1,3 @@
-import { composeWithDevTools } from '@redux-devtools/extension'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
@@ -27,7 +26,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
   reducer: persistedReducer,
-  devTools: process.env.NODE_ENV !== 'production' ? composeWithDevTools() : false,
+  devTools: process.env.NODE_ENV !== 'production',
   middleware: [thunk]
 })
 
