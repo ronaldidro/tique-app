a76bfa22d5abc9dbbfb163eea3e8896a68a934e4
ronaldidro <rdiaz@digital.gob.pe>
2023-05-14 23:15:00 -0500

    added vertical orientation prop

--- app/src/components/fields/RadioField.js
+++ app/src/components/fields/RadioField.js
@@ -1,32 +1,32 @@
-import { FormControl, FormErrorMessage, HStack, Radio, RadioGroup } from '@chakra-ui/react'
+import { Radio, RadioGroup, Stack } from '@chakra-ui/react'
 import { Field } from 'formik'
 import PropTypes from 'prop-types'
+import FormField from './FormField'
 
-const RadioField = ({ name, validate, options, ...inputProps }) => {
-  return (
-    <Field name={name} validate={validate}>
-      {({ field: { onChange, ...rest }, meta }) => (
-        <FormControl as="fieldset" isInvalid={meta.touched && meta.error} {...inputProps}>
-          <RadioGroup {...rest}>
-            <HStack justify="space-between" fontWeight="medium">
-              {options.map(({ value, label }, index) => (
-                <Radio key={index} onChange={onChange} value={value}>
-                  {label}
-                </Radio>
-              ))}
-            </HStack>
-          </RadioGroup>
-          <FormErrorMessage>{meta.error}</FormErrorMessage>
-        </FormControl>
-      )}
-    </Field>
-  )
-}
+const RadioField = ({ name, label, options, validate, verticalOrientation = false, ...inputProps }) => (
+  <Field name={name} validate={validate}>
+    {({ field: { onChange, ...rest }, meta }) => (
+      <FormField label={label} meta={meta} {...inputProps}>
+        <RadioGroup {...rest}>
+          <Stack direction={verticalOrientation ? 'column' : 'row'} justify="space-between">
+            {options.map(({ value, label }, index) => (
+              <Radio key={index} onChange={onChange} value={value}>
+                {label}
+              </Radio>
+            ))}
+          </Stack>
+        </RadioGroup>
+      </FormField>
+    )}
+  </Field>
+)
 
 RadioField.propTypes = {
   name: PropTypes.string,
+  label: PropTypes.string,
+  options: PropTypes.array,
   validate: PropTypes.func,
-  options: PropTypes.array
+  verticalOrientation: PropTypes.bool
 }
 
 export default RadioField
