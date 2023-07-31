8c6ae50884f59ec6e52a3cfdd7005349289b8576
ronaldidro <rdiaz@digital.gob.pe>
2023-07-30 20:10:30 -0500

    updated frontend proxy

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { isExpired } from 'react-jwt'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: '/api',
+  baseUrl: `${process.env.REACT_APP_API_URI}/api`,
   prepareHeaders: (headers, { getState }) => {
     const token = getState().user?.token
 
